5a023e05d3f4ad0726a7772ffd121d5ed1f8788f
Brijesh Pujara <pujara.brijesh1@gmail.com>
2022-07-14 09:18:29 +0100

    Looping through array to extract each student

--- directory.rb
+++ directory.rb
@@ -1,20 +1,25 @@
-student_count = 11
-#first we print the list of students in the academy
+# all the students are put into an array
+ students = [
+ "Dr. Hannibal Lecter",
+ "Darth Vader",
+ "Nurse Ratched",
+ "Michael Corleone",
+ "Alex DeLarge",
+ "The Wicked Witch of the West",
+ "Terminator",
+ "Freddy Krueger",
+ "The Joker",
+ "Joffrey Baratheon",
+ "Norman Bates"]
+
+# and then printed with array index
 puts "The students of Villains Academy"
-puts "---------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked With of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+puts "----------------"
+students.each do |student|
+    puts student
+end
 #finally, we pint the total number of students
-print "Overall, we have #{student_count} great students"
-#it's important that print() doesn't add new line characters
+print "Overall, we have #{students.count} great students"
+
 
 
