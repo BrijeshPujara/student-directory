92ae483883c2081a1f09accc15e2d6c0dbb973f6
Brijesh Pujara <pujara.brijesh1@gmail.com>
2022-07-14 15:04:54 +0100

    Interactive student directory implemented

--- directory.rb
+++ directory.rb
@@ -1,25 +1,55 @@
 # all the students are put into an array
  students = [
- "Dr. Hannibal Lecter",
- "Darth Vader",
- "Nurse Ratched",
- "Michael Corleone",
- "Alex DeLarge",
- "The Wicked Witch of the West",
- "Terminator",
- "Freddy Krueger",
- "The Joker",
- "Joffrey Baratheon",
- "Norman Bates"]
+ {name:"Dr. Hannibal Lecter", cohort: :november},
+ {name:"Darth Vader", cohort: :november},
+ {name:"Nurse Ratched", cohort: :november},
+ {name:"Michael Corleone", cohort: :november},
+ {name:"Alex DeLarge", cohort: :november},
+ {name:"The Wicked Witch of the West", cohort: :november},
+ {name:"Terminator", cohort: :november},
+ {name:"Freddy Krueger", cohort: :november},
+ {name:"The Joker", cohort: :november},
+ {name:"Joffrey Baratheon", cohort: :november},
+ {name:"Norman Bates", cohort: :november}
+]
 
 # and then printed with array index
-puts "The students of Villains Academy"
-puts "----------------"
-students.each do |student|
-    puts student
-end
-#finally, we pint the total number of students
-print "Overall, we have #{students.count} great students"
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit the return twice"
 
+  students = []
+
+  name = gets.chomp
+
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+
+    name = gets.chomp 
+  end
+
+  students
+end
+
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "----------------"
+end
+
+def print(names)
+  names.each do |name|
+    puts "#{name[:name]} cohort: (#{name[:cohort]})"
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+students = input_students
+print_header()
+print(students)
+print_footer(students)
 
 
