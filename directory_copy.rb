e15c3dff684c8a9e9de95e8b78627eb2c336f8c8
Brijesh Pujara <pujara.brijesh1@gmail.com>
2022-07-15 16:38:10 +0100

    load students method created

--- directory_copy.rb
+++ directory_copy.rb
@@ -85,6 +85,15 @@ def save_students
   end
   file.close
 end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
 interactive_menu()
 
 
